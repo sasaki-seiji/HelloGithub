6e811ff0ffb488ad9e113c80c2207933ef6d7551
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-03-13 16:56:23 +0900

    eclipseからGithubにコミット・プッシュするテスト

---
+++ src/HelloGithub.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : HelloGithub.cpp
+// Author      : sasaki seiji
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World!\n" ; // prints !!!Hello World!!!
+	return 0;
+}
